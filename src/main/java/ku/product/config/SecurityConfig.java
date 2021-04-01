1434730495fdc8b490d2e85ebb6cfd2915f1336b
Vilquid <60876471+Vilquid@users.noreply.github.com>
2021-04-01 11:31:29 +0700

    Enable CORS

--- src/main/java/ku/product/config/SecurityConfig.java
+++ src/main/java/ku/product/config/SecurityConfig.java
@@ -5,18 +5,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/api/products").permitAll()
-                .anyRequest()
-                .authenticated();
-
-    }
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
+	@Override
+	protected void configure(HttpSecurity http) throws Exception
+	{
+		http
+				.cors().and()
+				.csrf().disable()
+				.authorizeRequests()
+				.antMatchers("/api/products").permitAll()
+				.anyRequest()
+				.authenticated();
+	}
 }
